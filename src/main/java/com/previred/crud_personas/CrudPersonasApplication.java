f7f442a6ec3445383309970680fe6a69de45eb71
Hector Carrasco <mail@hcarrasco.cl>
2025-10-17 14:57:17 -0300

    Feat: adding spring boot project skeleton

---
+++ src/main/java/com/previred/crud_personas/CrudPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.previred.crud_personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPersonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/previred/crud_personas/CrudPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previred.crud_personas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
